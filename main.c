0ce65043e4d9b5a3558481d6777a449868665a92
Alex G <alex@alex.com>
2015-05-06 07:46:13 -0400

    Revert "create "askname.h""
    This reverts commit 748c64dd450195db831e1dead809b00018fd9965.

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
-/* main.c */
 #include <stdio.h>
-
-#include "askname.h"
+#include <string.h>
 
 int main(int argc, char **argv)
 {
     char first[25, last[255];
 
-    askname(first, last);
+    printf("Enter your first name: ");
+    fgets(first, 255, stdin);
+    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Hello, %s %s!\n", first, last);
+    printf("Now enter your last name: ");
+    gets(last); /* buffer overflow? what's that? */
+
+    printf("Hello %s %s!\n", first, last);
     return 0;
 }
